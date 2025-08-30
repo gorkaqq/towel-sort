2815f0f05d684d9391445740960530563460e483
gorkaqq <gorka1233@gmail.com>
2025-08-30 18:18:14 +0400

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  const result = [];
+  if (!matrix) return result;
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      const columnIdx = i % 2 === 0 ? j : matrix[i].length - j - 1;
+
+      result.push(matrix[i][columnIdx]);
+    }
+  }
+  return result;
 };
